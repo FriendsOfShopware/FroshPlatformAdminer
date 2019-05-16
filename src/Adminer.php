c9ef4f4d9e7a4fd89240868c4d240e577dd2bdce
Soner Sayakci <s.sayakci@gmail.com>
2019-05-16 21:07:19 +0200

    Some cleanup

--- src/Adminer.php
+++ src/Adminer.php
@@ -2,9 +2,7 @@
 
 namespace Frosh\Adminer;
 
-use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Plugin;
-use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
 class Adminer extends Plugin
 {
--- src/Controller/Adminer.php
+++ src/Controller/Adminer.php
@@ -1,13 +1,9 @@
 <?php
 
-
 namespace Frosh\Adminer\Controller;
 
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Shopware\Core\Framework\Util\Random;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,7 +12,7 @@ class Adminer extends AbstractController
     /**
      * @Route(path="/api/v1/frosh_adminer/login")
      */
-    public function index(Request $request)
+    public function index(Request $request): JsonResponse
     {
         error_reporting(0);
         ini_set('display_errors', 'off');
@@ -48,7 +44,7 @@ class Adminer extends AbstractController
         return $response;
     }
 
-    private function getDatabaseCredentials()
+    private function getDatabaseCredentials(): array
     {
         $db = parse_url(getenv('DATABASE_URL'));
         $db = array_map('rawurldecode', $db);
@@ -59,56 +55,4 @@ class Adminer extends AbstractController
 
         return $db;
     }
-
-    private function encrypt_string($str, $key) {
-        if ($str == "") {
-            return "";
-        }
-        $key = array_values(unpack("V*", pack("H*", md5($key))));
-        $v = $this->str2long($str, true);
-        $n = count($v) - 1;
-        $z = $v[$n];
-        $y = $v[0];
-        $q = floor(6 + 52 / ($n + 1));
-        $sum = 0;
-        while ($q-- > 0) {
-            $sum = $this->int32($sum + 0x9E3779B9);
-            $e = $sum >> 2 & 3;
-            for ($p=0; $p < $n; $p++) {
-                $y = $v[$p + 1];
-                $mx = $this->xxtea_mx($z, $y, $sum, $key[$p & 3 ^ $e]);
-                $z = $this->int32($v[$p] + $mx);
-                $v[$p] = $z;
-            }
-            $y = $v[0];
-            $mx = $this->xxtea_mx($z, $y, $sum, $key[$p & 3 ^ $e]);
-            $z = $this->int32($v[$n] + $mx);
-            $v[$n] = $z;
-        }
-        return $this->str2long($v, false);
-    }
-
-    function str2long($s, $w) {
-        $v = array_values(unpack('V*', str_pad($s, 4 * ceil(strlen($s) / 4), "\0")));
-        if ($w) {
-            $v[] = strlen($s);
-        }
-        return $v;
-    }
-
-
-    function int32($n) {
-        while ($n >= 2147483648) {
-            $n -= 4294967296;
-        }
-        while ($n <= -2147483649) {
-            $n += 4294967296;
-        }
-        return (int) $n;
-    }
-
-    function xxtea_mx($z, $y, $sum, $k) {
-        return $this->int32((($z >> 5 & 0x7FFFFFF) ^ $y << 2) + (($y >> 3 & 0x1FFFFFFF) ^ $z << 4)) ^ $this->int32(($sum ^ $y) + ($k ^ $z));
-    }
-
 }
