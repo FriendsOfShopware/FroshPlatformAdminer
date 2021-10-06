52e97652e46ddc0394c75297dc603aee4ad34ec1
plugware <plugware@users.noreply.github.com>
2021-10-06 15:11:38 +0200

    No direct ApiService import
    Use global Shopware object directly
    plugin-rules/no-src-imports

--- src/Resources/app/administration/src/core/service/api/adminer.service.js
+++ src/Resources/app/administration/src/core/service/api/adminer.service.js
@@ -1,4 +1,4 @@
-import ApiService from 'src/core/service/api.service';
+const ApiService = Shopware.Classes.ApiService;
 
 class AdminerService extends ApiService {
     constructor(httpClient, loginService, apiEndpoint = 'frosh_adminer') {
