3c0d86c2ca58c6f5a0b362c2578b99561907429f
maximilian-walter <maximilian-walter@users.noreply.github.com>
2026-06-12 17:38:16 +0200

    feat: allow to disable SSL verification
    Support DATABASE_SSL_DONT_VERIFY_SERVER_CERT env-variable to disable
    the verification of SSL certificates

--- src/Adminer/adminer-plugins.php
+++ src/Adminer/adminer-plugins.php
@@ -6,6 +6,9 @@ $ssl = [
     'ca' => EnvironmentHelper::getVariable('DATABASE_SSL_CA'),
     'cert' => EnvironmentHelper::getVariable('DATABASE_SSL_CERT'),
     'key' => EnvironmentHelper::getVariable('DATABASE_SSL_KEY'),
+    'verify' => EnvironmentHelper::hasVariable('DATABASE_SSL_DONT_VERIFY_SERVER_CERT')
+        ? !EnvironmentHelper::getVariable('DATABASE_SSL_DONT_VERIFY_SERVER_CERT')
+        : null,
 ];
 
 $sslConfig =  0 < count(array_filter($ssl)) ? $ssl : null;
